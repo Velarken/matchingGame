69623b34ba94fc304e9c3945938f483a9412a880
Velarken <181585418+Velarken@users.noreply.github.com>
2025-11-09 08:54:42 -0500

    added component logic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,38 @@
-export function Footer({}) {
+/* import GameTutorial from './GameTutorial'
+import { motion } from 'motion/react'
+import {useState} from 'react'
+import Volume  from '../assets/utility/volume.svg'
+import VolumeOff  from '../assets/utility/volume_off.svg'
+
+export function Footer({
+    isMusicPlaying,
+    setIsMusicPlaying,
+    isSoundPlaying,
+    setIsSoundPlaying,
+    playClick
+}) {
+    const [isInfoNeeded,setIsInfoNeeded] = useState(false);
     return (
         <>
-        
+            <motion.footer
+                initial={{opacity:0, y:100}}
+                animate={{opacity:1, y:0}}
+                transition={{duration:0.7}}
+            >
+                <div className="footerContainer">
+                    <div className="soundSection">
+                        <button onClick={() => {
+                            setIsSoundPlaying(!isSoundPlaying);
+                            playClick();
+                        }}>
+                            {isSoundPlaying
+                            ? <Volume className='svg'/>
+                            : <VolumeOff className='svg'/>}
+                        </button>
+                    </div>
+                </div>
+            </motion.footer>
         </>
     )
 }
-export default Footer;
+export default Footer; */
