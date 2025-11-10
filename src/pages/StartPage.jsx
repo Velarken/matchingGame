b288bfc81ce388d84e966faeea1b9355d9038fed
Velarken <181585418+Velarken@users.noreply.github.com>
2025-11-10 08:12:24 -0500

    imported asset

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,6 +1,8 @@
 import {motion} from 'motion/react'
 import logo from '../assets/logo.png'
 import startSound from '../assets/game_start.mp3'
+import '../StartPage.css';
+
 export function StartPage({setDifficultyLevel,playClick}) {
     const variants = {
         hidden: {scale:0},
@@ -36,19 +38,21 @@ export function StartPage({setDifficultyLevel,playClick}) {
                     initial='hidden'
                     animate='visible'
                 >
-                    <button onClick={() => {
-                        handlePlaySound();                        
-                        setDifficultyLevel([5,3]);
-                    }}
-                    >Easy</button>
-                    <button onClick={() => {
-                        handlePlaySound();
-                        setDifficultyLevel([7,4]);
-                    }}>Medium</button>
-                    <button onClick={() => {
-                        handlePlaySound();
-                        setDifficultyLevel([10,5]);
-                    }}>Hard</button>
+                    <div className='buttons'>
+                        <button onClick={() => {
+                            handlePlaySound();                        
+                            setDifficultyLevel([5,3]);
+                        }}
+                        >Easy</button>
+                        <button onClick={() => {
+                            handlePlaySound();
+                            setDifficultyLevel([7,4]);
+                        }}>Medium</button>
+                        <button onClick={() => {
+                            handlePlaySound();
+                            setDifficultyLevel([10,5]);
+                        }}>Hard</button>
+                    </div>
                 </motion.div>
             </div>
         </>
